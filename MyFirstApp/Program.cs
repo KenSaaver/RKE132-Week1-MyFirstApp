d2d03466cee3275f3fe65d54534d3d5afb7f7497
Ken Saaver <kenkaupo97@gmail.com>
2023-01-25 15:08:08 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,17 @@
+﻿//This is a comment to my code
+
+//rakendus küsib kasutajalt sisestada tema nime
+
+//rakendus tervistab kasutajat nimepidi
+
+Console.WriteLine("Enter your name");//Output
+
+//string - sõne
+string userName = ;//Input 
+
+Console.WriteLine("Hello" + ", " + userName + "!");//Output
+
+//string interpolation
+
+Console.WriteLine($"Hello, {userName}!");//Output
+
